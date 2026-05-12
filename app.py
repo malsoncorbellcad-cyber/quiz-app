0e476f486fd852db772bf56763ef280c1f13389f
Islamic kids <malsoncorbell.cad@gmail.com>
2026-05-12 21:29:55 +0500

    fix port

--- app.py
+++ app.py
@@ -133,4 +133,4 @@ def admin():
     return render_template("admin.html", results=results, quizzes=quizzes)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
